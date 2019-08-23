90fc8cca268ce10f485f9c231cfd65bfef4f6275
新亮 <xinliangnote@xin.com>
2019-08-23 13:07:46 +0800

    初始化

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-gin-api
+
+go 1.12
+
+require github.com/gin-gonic/gin v1.4.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
