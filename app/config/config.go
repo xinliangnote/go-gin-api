dc9a6b22f9bba1087310f16bbba168962ea8a6a2
新亮 <xinliangnote@163.com>
2019-08-29 19:36:12 +0800

    Add util.SendMail 方法（gomail）

--- app/config/config.go
+++ app/config/config.go
@@ -5,6 +5,23 @@ const (
 	AppPort = ":9999"
 	AppName = "go-gin-api"
 
+	// 超时时间
 	AppReadTimeout  = 120
 	AppWriteTimeout = 120
+
+	// 日志文件
+	AppAccessLogName = "log/" + AppName + "-access.log"
+	AppErrorLogName  = "log/" + AppName + "-error.log"
+
+	// 系统告警邮箱信息
+	SystemEmailUser = "xinliangnote@163.com"
+	SystemEmailPass = "" //密码或授权码
+	SystemEmailHost = "smtp.163.com"
+	SystemEmailPort = 465
+
+	// 告警接收人
+	ErrorNotifyUser = "xinliangnote@163.com"
+
+	// 告警开关 1=开通 -1=关闭
+	ErrorNotifyOpen = 1
 )
---
+++ app/util/mail.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"go-gin-api/app/config"
+	"gopkg.in/gomail.v2"
+	"strings"
+)
+
+func SendMail(mailTo string, subject string, body string) error {
+	
+	if config.ErrorNotifyOpen != 1 {
+		return nil
+	}
+
+	m := gomail.NewMessage()
+
+	//设置发件人
+	m.SetHeader("From", config.SystemEmailUser)
+
+	//设置发送给多个用户
+	mailArrTo := strings.Split(mailTo, ",")
+	m.SetHeader("To", mailArrTo...)
+
+	//设置邮件主题
+	m.SetHeader("Subject", subject)
+
+	//设置邮件正文
+	m.SetBody("text/html", body)
+
+	d := gomail.NewDialer(config.SystemEmailHost, config.SystemEmailPort, config.SystemEmailUser, config.SystemEmailPass)
+
+	err := d.DialAndSend(m)
+	if err != nil {
+		panic(err)
+	}
+	return err
+}
