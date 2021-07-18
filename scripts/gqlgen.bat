bb09c70a60b60410a3d9551aa5c0942a4d77bd10
anserme <lczr1026673211@gmail.com>
2021-07-18 22:28:06 +0800

    bug fix

--- scripts/gqlgen.bat
+++ scripts/gqlgen.bat
@@ -4,7 +4,7 @@ echo.
 echo Regenerating gqlgen file
 echo.
 del internal\graph\generated\generated.go internal\graph\model\generated.go internal\graph\resolvers\generated\generated.go
-go run -v github.com\99designs\gqlgen $1
+go run -v github.com/99designs/gqlgen $1
 echo.
 echo Done.
 echo.
